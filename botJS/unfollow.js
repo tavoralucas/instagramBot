ff3636ed76255163eaaf5273b9035c4502800439
Lucas Nascimento Távora <38147859+tavoralucas@users.noreply.github.com>
2020-05-03 00:12:49 -0300

    Create unfollow.js

---
+++ botJS/unfollow.js
@@ -0,0 +1,14 @@
+let cont = 0;
+document.querySelectorAll('._8A5w5').forEach((v,i) => {
+    setTimeout(() => {
+        if(v.innerText == 'Following') {
+            v.click();
+            document.querySelectorAll('.-Cab_').forEach((v,i) => {
+                if(v.innerText == "Unfollow"){
+                    v.click();
+                    cont++
+                }
+            })
+        }
+    }, i * 5000)
+})
